62741486492b3689e73a47dbc698fa4137104855
Antoine Groudiev <antoine.groudiev@gmail.com>
2022-02-06 18:10:16 +0100

    Ajouts de programmes et modification de la tâche

---
+++ source/hamming.ml
@@ -0,0 +1,16 @@
+let rec hamming n = 
+  if n = 2 || n = 3 || n = 5 then true
+  else 
+    if n mod 2 = 0 then (hamming (n/2))
+    else if n mod 3 = 0 then (hamming (n/3))
+    else if n mod 5 = 0 then (hamming (n/5))
+    else false;;
+
+print_int (Bool.to_int (hamming 9001));;
+
+let rec suite_recurrente (f, n, a) =
+  if n=0 then a
+  else f (suite_recurrente (f, n-1, a));;
+
+let f = fun x ->2*x;;
+print_int (suite_recurrente (f, 10, 1));;
